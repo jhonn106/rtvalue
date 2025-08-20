bb1b8615a2f5472a3afcb2dc89dd43c484cc9d3f
jhonn106 <jhonni.cool@gmail.com>
2025-08-20 09:28:43 +0700

    Delete stockbit-bot directory

--- stockbit-bot/clients/token_store.py
+++
@@ -1,7 +0,0 @@
-from auth.stockbit_login import get_bearer_token, load_token_if_valid
-
-def ensure_bearer():
-    token = load_token_if_valid()
-    if token:
-        return token
-    return get_bearer_token()
